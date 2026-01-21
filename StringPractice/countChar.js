b0e4c7f015c4b1b58079b9abf980fd37d7c9ac72
Vipin <vipinbhardwal286@gmail.com>
2026-01-21 23:57:50 +0530

    Count string length

---
+++ StringPractice/countChar.js
@@ -0,0 +1,48 @@
+let str = "hello this is js tutorial."
+
+// console.log(str.length)
+
+
+// let count = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//     count++
+// }
+// console.log(count)
+
+
+// let count = 0;
+// function countChr(str) {
+
+//     for (let i = 0; i < str.length; i++) {
+//          count++
+//     }
+//     return count
+// }
+
+
+// console.log(countChr(str))
+
+
+
+
+
+
+
+// let strLen = 0;
+
+// let i = 0;
+
+// while(str[i]!==undefined){
+//     strLen++;
+//     i++
+// }
+
+// console.log(strLen)
+
+
+
+
+
+
+
--- StringPractice/withIndexing.js
+++ StringPractice/withIndexing.js
@@ -1,16 +1,29 @@
-let str = "hello sir, how are you"
+// let str = "hello sir, how are you"
 
-let newStr = ""
+// let newStr = ""
 
-function replaceWith(str) {
-    for (let ch of str) {
-        if (ch === " ") {
-            newStr += "_"
-        }
-        newStr += ch
-    }
-    return newStr
-}
+// function replaceWith(str) {
+//     for (let ch of str) {
+//         if (ch === " ") {
+//             newStr += "_"
+//         }
+//         newStr += ch
+//     }
+//     return newStr
+// }
 
-console.log(replaceWith(str));
+// console.log(replaceWith(str));
+
+
+
+
+// let str = "hello sir, how are you"
+
+// let newStr = str.split(" ").join("_");
+
+// console.log(newStr);
+
+// let newStr = str.replaceAll(" ", "_");
+// console.log(newStr);
+
 
