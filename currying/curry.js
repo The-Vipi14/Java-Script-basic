8e3ff35b8c8c8dd85e21e0dc48cce28d2a20d2f5
Vipin <vipinbhardwal286@gmail.com>
2026-01-21 09:03:15 +0530

    Currying

---
+++ currying/curry.js
@@ -0,0 +1,27 @@
+// function add (a){
+//     return (b)=>{
+//         return a + b;
+//     }
+// }
+
+// console.log(add(5)(5));
+
+
+
+
+// (function IIF(){
+//     console.log("hello sir")
+// })()
+
+// Curried logging function
+const log = level => message => {
+  console.log(`[${level}] ${message}`);
+};
+
+// Create specific loggers
+const infoLog = log("INFO");
+const errorLog = log("ERROR");
+
+// Use them
+infoLog("User logged in");
+errorLog("Server crashed");
