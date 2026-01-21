85a7c984847e638a23c605b3904c392eaf30dd6d
Vipin <vipinbhardwal286@gmail.com>
2026-01-21 23:48:46 +0530

    IIFE

---
+++ IIFE/IIFE.js
@@ -0,0 +1,12 @@
+(()=>{
+    console.log("this is Immediate invoked function expression.")
+})();
+
+
+(function hello(){
+    console.log("this is Immediate invoked function expression.")
+})();
+
+
+
+
