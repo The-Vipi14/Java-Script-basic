a898c876335feacff24c4e711a0b84277b13cace
Vipin <vipinbhardwal286@gmail.com>
2026-01-21 23:44:39 +0530

    Closure

---
+++ closure/clouser.js
@@ -0,0 +1,14 @@
+function outer(currentBalance) {
+    let current = currentBalance
+    return (ammout) => {
+        return current = current + ammout
+    }
+}
+
+let vipin = outer(100000);
+
+let newAmount = vipin(200000);
+
+newAmount = vipin(500000);
+console.log(newAmount);
+
